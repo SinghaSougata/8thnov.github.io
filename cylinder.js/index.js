bfc09f4e1f2512dab447bf44dfa2487a56a6278a
SinghaSougata <91247187+SinghaSougata@users.noreply.github.com>
2021-11-15 13:55:48 +0530

    Add files via upload

---
+++ cylinder.js/index.js
@@ -0,0 +1,13 @@
+
+var cylinder= {
+    v: 4,
+    h: 7,
+    volume:
+     function(){
+        return Math.PI * this.v * this.v * this.h;
+    },
+    surface: 
+    function(){
+    return 2 * Math.PI * this.v * this.h;
+    }
+};
